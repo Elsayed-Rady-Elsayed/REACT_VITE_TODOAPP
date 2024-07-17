e7fdcea5dc2c8e4a77cc65a6465bca17ba69b6c0
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-07-18 00:27:44 +0300

    edit deployed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export const Header = (props) => {
   return (
     <>
-      <div className="header container m-auto p-2 flex justify-between items-center">
+      <div className="header container m-auto md:p-5 lg:p-5 flex justify-between items-center px-10 py-5">
         <Link to="/REACT_VITE_TODOAPP/">
           <h2
             className={`${
@@ -20,13 +20,13 @@ export const Header = (props) => {
         </Link>
         <div>
           <a href="https://github.com/Elsayed-Rady-Elsayed">
-            <i className="fa-brands fa-github hover:text-white transition-all"></i>
+            <i className="fa-brands fa-github text-gray-600 hover:text-white transition-all"></i>
           </a>
           <a
-            className="ms-2"
+            className="ms-3"
             href="https://www.linkedin.com/in/elsayed-rady-elsayed-73b541288/"
           >
-            <i className="fa-brands fa-linkedin hover:text-[#378fe9] transition-all"></i>
+            <i className="fa-brands fa-linkedin text-gray-600 hover:text-[#378fe9] transition-all"></i>
           </a>
         </div>
       </div>
