5a2e185c217bd6d73919a6f22c34ff0c22ae7e6f
Elsayed-Rady-Elsayed <sayedrady61@gmail.com>
2024-07-20 06:33:13 +0300

    finishd

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/REACT_VITE_TODOAPP/",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
+        },
+      },
+    },
+  },
 });
